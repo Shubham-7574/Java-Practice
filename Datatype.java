ac3cb80681f78c2b8f421aa5ab4cfa78aa676c76
Shubham-7574 <144315998+Shubham-7574@users.noreply.github.com>
2023-09-07 16:09:22 +0530

    Create Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,25 @@
+# Java-Practice
+  
+class Hello
+{
+    public static void main(String[] args)
+    {
+        byte by= 127;
+        int i = 5;
+        short sh=545;
+        long l=54545l;
+        float f= 3.2f;
+        double d=56.32;
+        char ch = 'A';
+        Boolean b= true;
+        System.out.println(by);
+        System.out.println(i);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(b);
+
+    }
+}
