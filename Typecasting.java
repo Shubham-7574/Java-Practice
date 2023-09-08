fbf747c3aa138af990af3968c591ea132fb0ee0a
Shubham-7574 <144315998+Shubham-7574@users.noreply.github.com>
2023-09-08 15:52:22 +0530

    Create Typecasting.java

---
+++ Typecasting.java
@@ -0,0 +1,11 @@
+# Java-Practice
+class Typecasting
+{
+    public static void main(String[] args)
+    {
+       // byte b = 100;
+        int i = 12;
+        byte b = (byte)i; // if we can not mention byte so its give compile time error
+                            //thats why we mention
+        System.out.println(b);
+    }
