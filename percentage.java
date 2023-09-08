0bb285bb9c3462482d64eec7b599ec029b3975e5
Shubham-7574 <144315998+Shubham-7574@users.noreply.github.com>
2023-09-08 14:37:05 +0530

    Create percentage.java

---
+++ percentage.java
@@ -0,0 +1,16 @@
+# Java-Practice
+import java.util.Scanner;
+
+class Percentage
+
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter marks");
+        double mark = sc.nextDouble();
+        double mark1=mark;
+        double per= mark1 /5;
+        System.out.println(per);
+    }
+}
