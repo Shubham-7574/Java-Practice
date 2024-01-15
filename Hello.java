d579e9f323fff4a7a94535280f1b3033da889d41
Shubham-7574 <144315998+Shubham-7574@users.noreply.github.com>
2024-01-15 09:44:07 +0530

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,14 +1,12 @@
-import java.util.Scanner;
+package Test;
 
-class Hello
+public class Hello {
+
+    public static void main (String[] args)
 {
-    public static void main(String[] args)
-    {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter first no.");
-        int num1=sc.nextInt();
-        System.out.println("Enter second no.");
-        int num2=sc.nextInt();
-        System.out.println(num1+num2);
-    }
+    System.out.println("Hello");
 }
+}
+
+
+
